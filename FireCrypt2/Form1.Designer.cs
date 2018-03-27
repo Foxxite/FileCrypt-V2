d350824535fd2dc224d49382cb6b541541f74920
Foxxite <Dutch4Minecrafters@gmail.com>
2018-03-27 12:34:12 +0200

    Initial Commit

---
+++ FireCrypt2/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace FireCrypt2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(232, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 71);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Encrypt File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Aqua;
+            this.linkLabel1.Location = new System.Drawing.Point(404, 185);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "GitHub";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FireCrypt2.Properties.Resources.index;
+            this.pictureBox1.Location = new System.Drawing.Point(77, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(321, 79);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Status: idle";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "*Password: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '웃';
+            this.textBox1.Size = new System.Drawing.Size(363, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(456, 207);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(476, 250);
+            this.MinimumSize = new System.Drawing.Size(476, 250);
+            this.Name = "Form1";
+            this.Text = "FileCrypt 2.0";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FireCrypt2/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FireCrypt2
+{
+    public partial class Form1 : Form
+    {
+        public readonly byte[] salt = new byte[8] { 0x50, 0x3f, 0xc8, 0xa4, 0xf1, 0xf0, 0xab, 0x45 }; // Must be at least eight bytes.  MAKE THIS SALTIER!
+        public const int iterations = 2048; // Recommendation is >= 1000.
+        public string sourceFilename;
+        public string destinationFilename;
+        public string password;
+        public string realEx;
+        public string ext;
+        public bool Encrypt;
+
+        private GifImage gifImage = null;
+        private string filePath = @"C:\Users\Jeremy\Desktop\ExampleAnimation.gif";
+
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            pictureBox1.Visible = false;
+            filePath = pictureBox1.ImageLocation;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.github.com/Foxxite");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "All files (*.*)|*.*|Encrypted Files (*.🦊🔒)|*.🦊🔒";
+            openFileDialog1.Title = "Open an Encrypted/Decerpted File";
+            openFileDialog1.ShowDialog();
+
+            sourceFilename = openFileDialog1.FileName;
+
+            this.button1.Text = "Selected File:\n" + sourceFilename;
+
+            ext = Path.GetExtension(openFileDialog1.FileName);
+
+            if (ext == ".🦊🔒")
+            {
+                label1.Text = "Status: Ready to decrypt file";
+                button2.Enabled = true;
+                button2.Text = "Decrypt File";
+                Encrypt = false;
+            }
+            else
+            {
+                button2.Enabled = true;
+                button2.Text = "Encrypt File";
+                label1.Text = "Status: Ready to encrypt file";
+                Encrypt = true;
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBox1.Text))
+            {
+                password = textBox1.Text;
+                if(Encrypt)
+                {
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    timer1.Enabled = true;
+                    pictureBox1.Visible = true;
+                    label1.Text = "Status: Encrypting file...";
+                    try
+                    {
+                        destinationFilename = sourceFilename + ".🦊🔒";
+                        await EncryptFile(sourceFilename, destinationFilename, password, salt, iterations);
+                        File.Delete(sourceFilename);
+                    }
+                    catch (Exception ex)
+                    {
+                        SystemSounds.Exclamation.Play();
+                        MessageBox.Show(ex.Message, "FileCrypt 2.0 | Error");
+                    }
+                    timer1.Enabled = false;
+                    pictureBox1.Visible = false;
+                    SystemSounds.Hand.Play();
+                    MessageBox.Show("Your file: `" + sourceFilename + "`\nhas been successfully encrypted to: `" + sourceFilename + ".🦊🔒`", "FileCrypt 2.0 | Success");
+                    button1.Enabled = true;
+                    button1.Text = "Choose File";
+                    label1.Text = "Status: Idle";
+                    sourceFilename = null;
+                }
+                else
+                {
+                    destinationFilename = sourceFilename;
+
+                    string NewDestinationFilename = destinationFilename;
+
+                    int index = NewDestinationFilename.IndexOf(".🦊🔒");
+
+                    if (index != -1)
+                    {
+                        NewDestinationFilename = NewDestinationFilename.Remove(index);
+                    }
+
+                    destinationFilename = NewDestinationFilename;
+
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    timer1.Enabled = true;
+                    pictureBox1.Visible = true;
+                    label1.Text = "Status: Decrypting file...";
+                    try
+                    {
+                        await DecryptFile(sourceFilename, destinationFilename, password, salt, iterations);
+                        File.Delete(sourceFilename);
+                    }
+                    catch (Exception ex)
+                    {
+                        SystemSounds.Exclamation.Play();
+                        MessageBox.Show(ex.Message, "FileCrypt 2.0 | Error");
+                    }
+                    timer1.Enabled = false;
+                    pictureBox1.Visible = false;
+                    SystemSounds.Hand.Play();
+                    MessageBox.Show("Your file: `" + sourceFilename + "`\nhas been successfully decrypted to: `" + destinationFilename + "`", "FileCrypt 2.0 | Success");
+                    button1.Enabled = true;
+                    button1.Text = "Choose File";
+                    label1.Text = "Status: Idle";
+                    sourceFilename = null;
+                }
+            }
+            else
+            {
+                SystemSounds.Exclamation.Play();
+                MessageBox.Show("You forgot to enter a password", "FileCrypt 2.0 | No Password");
+            }
+            
+
+        }
+
+        public async Task DecryptFile(string sourceFilename, string destinationFilename, string password, byte[] salt, int iterations)
+        {
+            AesManaged aes = new AesManaged();
+            aes.BlockSize = aes.LegalBlockSizes[0].MaxSize;
+            aes.KeySize = aes.LegalKeySizes[0].MaxSize;
+            // NB: Rfc2898DeriveBytes initialization and subsequent calls to   GetBytes   must be eactly the same, including order, on both the encryption and decryption sides.
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, salt, iterations);
+            aes.Key = key.GetBytes(aes.KeySize / 8);
+            aes.IV = key.GetBytes(aes.BlockSize / 8);
+            aes.Mode = CipherMode.CBC;
+            ICryptoTransform transform = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using (FileStream destination = new FileStream(destinationFilename, FileMode.CreateNew, FileAccess.Write, FileShare.None))
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(destination, transform, CryptoStreamMode.Write))
+                {
+                    try
+                    {
+                        using (FileStream source = new FileStream(sourceFilename, FileMode.Open, FileAccess.Read, FileShare.Read))
+                        {
+                            source.CopyTo(cryptoStream);
+                        }
+                    }
+                    catch (CryptographicException exception)
+                    {
+                        if (exception.Message == "Padding is invalid and cannot be removed.")
+                            throw new ApplicationException("Universal Microsoft Cryptographic Exception (Not to be believed!)", exception);
+                        else
+                            throw;
+                    }
+                }
+            }
+        }
+
+        public async Task EncryptFile(string sourceFilename, string destinationFilename, string password, byte[] salt, int iterations)
+        {
+            AesManaged aes = new AesManaged();
+            aes.BlockSize = aes.LegalBlockSizes[0].MaxSize;
+            aes.KeySize = aes.LegalKeySizes[0].MaxSize;
+            // NB: Rfc2898DeriveBytes initialization and subsequent calls to   GetBytes   must be eactly the same, including order, on both the encryption and decryption sides.
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, salt, iterations);
+            aes.Key = key.GetBytes(aes.KeySize / 8);
+            aes.IV = key.GetBytes(aes.BlockSize / 8);
+            aes.Mode = CipherMode.CBC;
+            ICryptoTransform transform = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using (FileStream destination = new FileStream(destinationFilename, FileMode.CreateNew, FileAccess.Write, FileShare.None))
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(destination, transform, CryptoStreamMode.Write))
+                {
+                    try
+                    {
+                        using (FileStream source = new FileStream(sourceFilename, FileMode.Open, FileAccess.Read, FileShare.Read))
+                        {
+                            source.CopyTo(cryptoStream);
+                        }
+                    }
+                    catch (CryptographicException exception)
+                    {
+                        if (exception.Message == "Padding is invalid and cannot be removed.")
+                            throw new ApplicationException("Universal Microsoft Cryptographic Exception (Not to be believed!)", exception);
+                        else
+                            throw;
+                    }
+                }
+            }
+        }
+
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Image = gifImage.GetNextFrame();
+        }
+
+    }
+}
